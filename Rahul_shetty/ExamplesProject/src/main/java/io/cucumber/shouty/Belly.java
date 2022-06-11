9518792197738e8c96d7986e28567e4fb968d2d0
Jan Jurado <jandaniel.jurado@hotmail.com>
2022-06-11 00:35:55 -0500

    Created new folder to apply rahulshetty concepts

---
+++ Rahul_shetty/ExamplesProject/src/main/java/io/cucumber/shouty/Belly.java
@@ -0,0 +1,7 @@
+package io.cucumber.shouty;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+}
---
+++ Rahul_shetty/ExamplesProject/src/main/java/io/cucumber/shouty/Person.java
@@ -0,0 +1,20 @@
+package io.cucumber.shouty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    public void moveTo(Integer distance){
+
+    }
+
+    public void shout(String message) {
+
+    }
+
+    public List<String> getMessageHeard() {
+        List<String> result = new ArrayList<String>();
+        result.add("free bagels at Sean's");
+        return result;
+    }
+}
